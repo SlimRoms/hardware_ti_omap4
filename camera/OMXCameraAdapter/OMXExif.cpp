CyanogenMod/android_hardware_ti_omap4

camera: add Pixel{X,Y}Dimension and Orientation EXIF tags
These tags are needed by the CM13 camera to not crop images to square
when shooting portrait. As the camera HAL already rotated the picture,
always set orientation to 0.

Change-Id: Id8b6aac5369ae3adfdfec0ec7528edc849bd3b10

camera/OMXCameraAdapter/OMXExif.cpp
@@ -237,7 +237,7 @@ status_t OMXCameraAdapter::setupEXIF()
     struct timeval sTv;
     struct tm *pTime;
     OMXCameraPortParameters * capData = NULL;
-    CameraBuffer *memmgr_buf_array;
+    CameraBuffer *memmgr_buf_array = NULL;
     int buf_size = 0;
 
     LOG_FUNCTION_NAME;
@@ -371,6 +371,28 @@ status_t OMXCameraAdapter::setupEXIF()
              {
              exifTags->ulImageHeight = capData->mHeight;
              exifTags->eStatusImageHeight = OMX_TI_TagUpdated;
+             }
+
+         if ( OMX_TI_TagReadWrite == exifTags->eStatusPixelXDimension &&
+              OMX_TI_TagReadWrite == exifTags->eStatusPixelYDimension)
+             {
+             if (mPictureRotation == 90 || mPictureRotation == 270) {
+                 exifTags->ulPixelXDimension = capData->mHeight;
+                 exifTags->ulPixelYDimension = capData->mWidth;
+                 }
+             else
+                 {
+                 exifTags->ulPixelXDimension = capData->mWidth;
+                 exifTags->ulPixelYDimension = capData->mHeight;
+                 }
+             exifTags->eStatusPixelXDimension = OMX_TI_TagUpdated;
+             exifTags->eStatusPixelYDimension = OMX_TI_TagUpdated;
+             }
+
+         if ( OMX_TI_TagReadWrite == exifTags->eStatusOrientation)
+             {
+             exifTags->usOrientation = 0;
+             exifTags->eStatusOrientation = OMX_TI_TagUpdated;
              }
 
          if ( ( OMX_TI_TagReadWrite == exifTags->eStatusGpsLatitude ) &&
@@ -560,14 +582,20 @@ status_t OMXCameraAdapter::setupEXIF_libjpeg(ExifElementsTable* exifTable,
 
     if ((NO_ERROR == ret)) {
         char temp_value[5];
-        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%lu", capData->mWidth);
+        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%lu", (unsigned long)capData->mWidth);
         ret = exifTable->insertElement(TAG_IMAGE_WIDTH, temp_value);
+        if ((NO_ERROR == ret)) {
+            ret = exifTable->insertElement(TAG_EXIF_IMAGE_WIDTH, temp_value);
+        }
      }
 
     if ((NO_ERROR == ret)) {
         char temp_value[5];
-        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%lu", capData->mHeight);
+        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%lu", (unsigned long)capData->mHeight);
         ret = exifTable->insertElement(TAG_IMAGE_LENGTH, temp_value);
+        if ((NO_ERROR == ret)) {
+            ret = exifTable->insertElement(TAG_EXIF_IMAGE_LENGTH, temp_value);
+        }
      }
 
     if ((NO_ERROR == ret) && (mEXIFData.mGPSData.mLatValid)) {

SlimRoms/hardware_ti_omap4

camera/OMXCameraAdapter/OMXExif.cpp
@@ -373,6 +373,28 @@ status_t OMXCameraAdapter::setupEXIF()
              exifTags->eStatusImageHeight = OMX_TI_TagUpdated;
              }
 
+         if ( OMX_TI_TagReadWrite == exifTags->eStatusPixelXDimension &&
+              OMX_TI_TagReadWrite == exifTags->eStatusPixelYDimension)
+             {
+             if (mPictureRotation == 90 || mPictureRotation == 270) {
+                 exifTags->ulPixelXDimension = capData->mHeight;
+                 exifTags->ulPixelYDimension = capData->mWidth;
+                 }
+             else
+                 {
+                 exifTags->ulPixelXDimension = capData->mWidth;
+                 exifTags->ulPixelYDimension = capData->mHeight;
+                 }
+             exifTags->eStatusPixelXDimension = OMX_TI_TagUpdated;
+             exifTags->eStatusPixelYDimension = OMX_TI_TagUpdated;
+             }
+
+         if ( OMX_TI_TagReadWrite == exifTags->eStatusOrientation)
+             {
+             exifTags->usOrientation = 0;
+             exifTags->eStatusOrientation = OMX_TI_TagUpdated;
+             }
+
          if ( ( OMX_TI_TagReadWrite == exifTags->eStatusGpsLatitude ) &&
               ( mEXIFData.mGPSData.mLatValid ) )
             {
@@ -560,14 +582,20 @@ status_t OMXCameraAdapter::setupEXIF_libjpeg(ExifElementsTable* exifTable,
 
     if ((NO_ERROR == ret)) {
         char temp_value[5];
-        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%u", capData->mWidth);
+        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%u", (unsigned long)capData->mWidth);
         ret = exifTable->insertElement(TAG_IMAGE_WIDTH, temp_value);
+        if ((NO_ERROR == ret)) {
+            ret = exifTable->insertElement(TAG_EXIF_IMAGE_WIDTH, temp_value);
+        }
      }
 
     if ((NO_ERROR == ret)) {
         char temp_value[5];
-        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%u", capData->mHeight);
+        snprintf(temp_value, sizeof(temp_value)/sizeof(char), "%u", (unsigned long)capData->mHeight);
         ret = exifTable->insertElement(TAG_IMAGE_LENGTH, temp_value);
+        if ((NO_ERROR == ret)) {
+            ret = exifTable->insertElement(TAG_EXIF_IMAGE_LENGTH, temp_value);
+        }
      }
 
     if ((NO_ERROR == ret) && (mEXIFData.mGPSData.mLatValid)) {