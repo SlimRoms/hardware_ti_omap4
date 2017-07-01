8b2f8f136e4d6e270c559b57182c95a8e12a2a98
Andreas K <a-v-a-t-a-r@gmx.net>
2017-07-01 13:47:00 +0200

    BUGBASH-733 Memory leak in system_server caused by suspend / color fade
    This fixes a memory leak in system_server which is triggered by switching
    the device to standby / waking it up.
    
    Change-Id: I17e330c62a8036b8349bd0ecf4ca7e0e0ebb373d

--- omap4.mk
+++ omap4.mk
@@ -47,5 +47,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.bq.gpu_to_cpu_unsupported=1
 
+# affects lock screen ColorFader and ensures release of /dev/pvrsrvkm GPU ressources
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.egl.destroy_after_detach=true
+
 $(call inherit-product, hardware/ti/omap4/common.mk)
 $(call inherit-product-if-exists, vendor/ti/omap4/omap4-vendor.mk)
